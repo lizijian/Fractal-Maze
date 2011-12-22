c73778816e568362f170f45cfa6dab6c54ccc1db
Jet Lee <ijetlee@gmail.com>
2011-12-22 23:20:31 +0800

    Add README and Licence declaration.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
-#include <QtCore/QTextCodec>
 
 int main(int argc, char *argv[])
 {
-    QTextCodec::setCodecForLocale(QTextCodec::codecForName("gbk"));
-    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("gbk"));
-    QTextCodec::setCodecForTr(QTextCodec::codecForName("gbk"));
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
